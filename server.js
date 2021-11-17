5ca4508fae53017048a996f7ac016ff8b015dad8
evanhosni <evankhaledhosni@gmail.com>
2021-11-16 17:47:48 -0800

    fixing heroku push

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MOGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
